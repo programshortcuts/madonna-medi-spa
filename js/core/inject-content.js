a637a7351bc451db14dc29bb76d6255a9765387e
tt <tt@Mac.lan>
2026-07-22 05:21:38 -0400

    main

--- js/core/inject-content.js
+++ js/core/inject-content.js
@@ -1,8 +1,8 @@
 // inject-content.js
 import { initAllVideos } from "../video/video-controls.js";
 // 🔥 Ensure correct default page
-// export const DEFAULT_PAGE = "pages/home/home.html";
-export const DEFAULT_PAGE = "pages/medical-spa-services/medical-spa-services.html";
+export const DEFAULT_PAGE = "pages/home/home.html";
+// export const DEFAULT_PAGE = "pages/medical-spa-services/medical-spa-services.html";
 // gitpsexport const DEFAULT_PAGE = "pages/contact/contact.html";
 // export const DEFAULT_PAGE = "pages/medical-spa-services/services/glp-1/glp-1.html";
 // export const DEFAULT_PAGE = "pages/products/products.html";
@@ -61,11 +61,17 @@ export function initInjectContentListeners() {
     // ✅ FIX: ensure DOM is ready before first injection
     requestAnimationFrame(() => {
 
-        history.replaceState(
-            { href: DEFAULT_PAGE },
-            "",
-            `#${DEFAULT_PAGE}`
-        );
+        if (!history.state) {
+
+            history.replaceState(
+                { href: DEFAULT_PAGE },
+                "",
+                `#${DEFAULT_PAGE}`
+            );
+        
+            injectPage(DEFAULT_PAGE);
+        
+        }
 
         injectPage(DEFAULT_PAGE);
 
@@ -80,12 +86,16 @@ export function initInjectContentListeners() {
         }
         e.preventDefault();
 
-        history.pushState(
-            { href },
-            "",
-            `#${href}`
-        );
+        if (history.state?.href !== href) {
 
+            history.pushState(
+                { href },
+                "",
+                `#${href}`
+            );
+        
+        }
+            
         injectPage(href);
         window.scrollTo(0, 0);
         mainLandingPage.scrollTo(0, 0);
@@ -117,6 +127,7 @@ export function initInjectContentListeners() {
 /* ----------------------------- PAGE INJECTION CORE
 ----------------------------- */
 export async function injectPage(href) {
+    console.log(history.state)
     if (!href) return;
     if (!isSafePath(href)) {
         console.warn('Blocked unsafe path:', href);
