aee024285a527eae5db69c048283253ad831bbe9
tt <tt@MacBookPro.lan>
2026-07-30 09:22:44 -0400

    main

--- js/nav/letter-nav.js
+++ js/nav/letter-nav.js
@@ -56,6 +56,7 @@ function buildElements(container = document) {
 
     return raw.filter(el => {
         if (!isActuallyVisible(el)) return false;
+        if (el.classList.contains('main-landing-page')) return false;
         if (seen.has(el)) return false;
 
         ensureFocusable(el);
@@ -108,6 +109,8 @@ export function letterNav({ container, e }) {
             sorted[0]; // wrap
     }
     if (!target?.el) return;
+    console.log(target.el);
+    
     focusNav({ e, target })
 
 }
--- js/ui/drop-down-home.js
+++ js/ui/drop-down-home.js
@@ -115,3 +115,4 @@ export function initDropDownHome() {
         }
     }
 }
+// function 
