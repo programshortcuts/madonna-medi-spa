31a7c453078b4f12e8f95f408dd77a46357a18ca
tt <tt@Mac.lan>
2026-08-04 16:23:25 -0400

    main

--- js/nav/keyboard-nav.js
+++ js/nav/keyboard-nav.js
@@ -1,29 +1,19 @@
 // letter-nav.js
-
 import { letterNav } from "./letter-nav.js";
 import { servicesSwiper } from "../visuals/swiper.js";
 export function initKeydboardNav({ container = document } = {}) {
     let lastElClicked = null;
-
-
     document.addEventListener('keydown', (e) => {
-
         if (e.key === 'Enter') {
-
             const index = Number(e.target.dataset.slide);
-
             if (!Number.isNaN(index)) {
-
                 servicesSwiper.slideToLoop(index);
-
                 if (e.target === lastElClicked) {
                     setTimeout(() => {
-
                         const activeSlide = document.querySelector(
                             '.services-swiper .swiper-slide-active'
                         );
                         activeSlide?.focus();
-
                     }, 50);
                 }
             }
